e3bdbec063dd2807e00acfde79bced3bd6e82783
AguiarVicente <aguiar14564@gmail.com>
2020-05-25 22:21:16 -0300

    modulo 7 - ESNext (realizado no dia 25/05/2020) - esperimento do operador pipeline

--- 07_ESNext/15_operador_pipeline.js
+++ 07_ESNext/15_operador_pipeline.js
@@ -1,7 +1,7 @@
-
 const dobro = (n) => n * 2
 const triplo = (n) => n * 3
 
+// chamada de função de dentro para fora 1º dobro, 2º dobro 3º triplo
 // console.log(5 |> dobro |> dobro |> triplo)
 
 console.log(triplo(dobro(dobro(2))))
