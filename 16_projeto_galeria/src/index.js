5ccf3485e480874fcc761660ee72814587b2205e
AguiarVicente <aguiar14564@gmail.com>
2020-07-18 23:37:45 -0300

    modulo 16 - galeria de fotos

--- 16_projeto_galeria/src/index.js
+++ 16_projeto_galeria/src/index.js
@@ -1,2 +1,10 @@
 // Arquivos SASS
 import './scss/index.scss'
+
+// dependencias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos
+import './js/core/includes'
+import './js/plugins/cityButtons'
