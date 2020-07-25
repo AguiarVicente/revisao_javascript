0bec1b1c4c79fc991ec64792390a9c752396df12
AguiarVicente <aguiar14564@gmail.com>
2020-07-25 18:23:40 -0300

    modulo 23 - projeto vue + electron - most-used-word

---
+++ 23_projeto_vue_electron/most-used-words/src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-main fluid>
+    <v-form>
+      <v-file-input 
+        label="Selecione as legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple 
+        chips 
+        v-model="files"
+        @click:append-outer="proccessSubtitles">
+      </v-file-input>   
+    </v-form>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
+    </div>
+  </v-main>
+</template>
+
+<script>
+import { ipcRenderer } from 'electron';
+import Pill from './Pill';
+export default {
+  components: {Pill},
+  data: function () {
+    return {
+      files: [],
+      groupedWords: []
+    }
+  },
+  methods: {
+    proccessSubtitles(){
+      const paths = this.files.map(f => f.path)
+      ipcRenderer.send('process-subtitle', paths)
+      ipcRenderer.on('process-subtitle', (event, resp) => {
+        this.groupedWords = resp
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
