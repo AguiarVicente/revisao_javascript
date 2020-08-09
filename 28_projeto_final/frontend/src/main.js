d5382c07c1fb81bcd1719466c6b3a1bba8569e76
AguiarVicente <aguiar14564@gmail.com>
2020-08-09 18:48:23 -0300

    modulo 28 - projeto final - criado componente home e stat

--- 28_projeto_final/frontend/src/main.js
+++ 28_projeto_final/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// configurar token emporario
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6IkpvYW8iLCJlbWFpbCI6ImpvYW9AZ21haWwuY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE1OTcwMDkwNTgsImV4cCI6MTU5NzI2ODI1OH0.XoLm-tEJ-91G8431inspKk35R7H3yZgJ5f6SVhvfOis'
+
 new Vue({
   store,
   router,
