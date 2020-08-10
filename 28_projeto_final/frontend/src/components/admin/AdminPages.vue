596f2a587d43ce31e065dc2a9007c961bdf77eaa
AguiarVicente <aguiar14564@gmail.com>
2020-08-09 22:32:30 -0300

    modulo 28 - projeto final - finalizado o cadastro de usuário

--- 28_projeto_final/frontend/src/components/admin/AdminPages.vue
+++ 28_projeto_final/frontend/src/components/admin/AdminPages.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="admin-page">
     <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastro & Cia" />
+    <div class="admin-pages-tabs">
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Artigos" active>
+            <ArticleAdmin />
+          </b-tab>
+          <b-tab title="Categorias">
+            <CategoryAdmin />
+          </b-tab>
+          <b-tab title="Usuários">
+            <UserAdmin />
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
+import ArticleAdmin from './ArticleAdmin'
+import UserAdmin from './UserAdmin'
+import CategoryAdmin from './CategoryAdmin'
 import PageTitle from '../template/PageTitle'
 export default {
   name: 'AdminPages',
-  components: { PageTitle }
+  components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
 }
 </script>
 
--- 28_projeto_final/frontend/src/main.js
+++ 28_projeto_final/frontend/src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
 
 // configurar token emporario
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6IkpvYW8iLCJlbWFpbCI6ImpvYW9AZ21haWwuY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE1OTcwMDkwNTgsImV4cCI6MTU5NzI2ODI1OH0.XoLm-tEJ-91G8431inspKk35R7H3yZgJ5f6SVhvfOis'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6Ik5pbCBBZ3VpciIsImVtYWlsIjoiYWd1aWFyMTQ1NjRAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU5NzAxODYzMywiZXhwIjoxNTk3Mjc3ODMzfQ.kAzYmZY9i6PBR8_cDroIPoxgZoAo8I64N935CtYGgWI'
 
 new Vue({
   store,
