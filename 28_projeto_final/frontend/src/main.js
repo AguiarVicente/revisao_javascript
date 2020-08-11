d1c7ba6fa99995cbff319f403c06fc154b8e4fa1
AguiarVicente <aguiar14564@gmail.com>
2020-08-10 23:06:48 -0300

    modulo 28 - projeto final - criado artigos e paginação

--- 28_projeto_final/frontend/src/main.js
+++ 28_projeto_final/frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // configurar token emporario
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6Ik5pbCBBZ3VpciIsImVtYWlsIjoiYWd1aWFyMTQ1NjRAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU5NzAxODYzMywiZXhwIjoxNTk3Mjc3ODMzfQ.kAzYmZY9i6PBR8_cDroIPoxgZoAo8I64N935CtYGgWI'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6Ik5pbCBBZ3VpciIsImVtYWlsIjoiYWd1aWFyMTQ1NjRAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU5NzEwOTYxMiwiZXhwIjoxNTk3MzY4ODEyfQ.sUOQfAgbVOnmJAT-vjCr1y-eJ-chtpuk_9XTWPXqXo0'
 
 new Vue({
   store,
