e76bcba45a4e7b1bf8dafa5aadd774461520e1ca
AguiarVicente <aguiar14564@gmail.com>
2020-08-11 22:00:33 -0300

    modulo 28 - projeto final - selecionando artigos por rotas e categorias

--- 28_projeto_final/frontend/src/main.js
+++ 28_projeto_final/frontend/src/main.js
@@ -11,7 +11,9 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // configurar token emporario
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6Ik5pbCBBZ3VpciIsImVtYWlsIjoiYWd1aWFyMTQ1NjRAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU5NzEwOTYxMiwiZXhwIjoxNTk3MzY4ODEyfQ.sUOQfAgbVOnmJAT-vjCr1y-eJ-chtpuk_9XTWPXqXo0'
+import axios from 'axios'
+axios.defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6Ik5pbCBBZ3VpciIsImVtYWlsIjoiYWd1aWFyMTQ1NjRAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU5NzE5MDUyMywiZXhwIjoxNTk3NDQ5NzIzfQ.xa5puu9Bm_RehXmuD9C21YWc3tOtstnBeLUKjwBOWqY'
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 
 new Vue({
   store,
